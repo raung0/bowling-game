26f329b4e7de3f57b03b12c3b4506057fe8ec73c
Radu Ungureanu <radu.ungureanu@socopon.com>
2026-05-05 23:07:17 +0300

    Add main menu
    Signed-off-by: Radu Ungureanu <radu.ungureanu@socopon.com>

--- rust/crates/gdext/src/game_state.rs
+++ rust/crates/gdext/src/game_state.rs
@@ -1,10 +1,30 @@
-use godot::prelude::*;
+use getset::Getters;
+use godot::{classes::Button, prelude::*};
 
-#[derive(GodotClass)]
+use crate::ui_manager::UiManager;
+
+#[derive(Clone, Copy)]
+pub enum Screen {
+    MainMenu,
+    Host,
+    Game,
+}
+
+impl Default for Screen {
+    fn default() -> Self {
+        Screen::MainMenu
+    }
+}
+
+#[derive(GodotClass, Getters)]
 #[class(base=Node)]
-struct GameState {
+pub struct GameState {
+    #[getset(get = "pub")]
     is_mobile: bool,
+    #[getset(get = "pub")]
     accel: Vector3,
+    #[getset(get = "pub", set = "pub")]
+    screen: Screen,
 
     base: Base<Node>,
 }
@@ -22,17 +42,41 @@ impl INode for GameState {
             base,
             is_mobile,
             accel: Vector3::ZERO,
+            screen: Screen::default(),
         }
+    }
+
+    fn ready(&mut self) {
+        self.base_mut().set_process(true);
+
+        let mut join_button = self.base().get_node_as::<Button>(
+            "UiManager/CenterContainer/VBoxContainer/Mobile/VBoxContainer/Join",
+        );
+        let mut create_button = self.base().get_node_as::<Button>(
+            "UiManager/CenterContainer/VBoxContainer/Desktop/VBoxContainer/Create",
+        );
+        let mut spectate_button = self.base().get_node_as::<Button>(
+            "UiManager/CenterContainer/VBoxContainer/Desktop/VBoxContainer/Spectate",
+        );
+
+        join_button.connect("pressed", &self.base().callable("on_join_pressed"));
+        create_button.connect("pressed", &self.base().callable("on_create_pressed"));
+        spectate_button.connect("pressed", &self.base().callable("on_spectate_pressed"));
     }
 
     fn process(&mut self, _delta: f64) {
         #[cfg(target_arch = "wasm32")]
-        let accel = browser_accel();
+        let accel = WASM::browser_accel();
 
         #[cfg(not(target_arch = "wasm32"))]
         let accel = Vector3::ZERO;
 
         self.accel = accel;
+
+        let mut ui_manager = self.base_mut().get_node_as::<UiManager>("UiManager");
+        ui_manager
+            .bind_mut()
+            .set_screen(self.screen, self.is_mobile);
     }
 }
 
@@ -46,26 +90,47 @@ impl GameState {
     }
 }
 
-#[cfg(target_arch = "wasm32")]
-use wasm_bindgen::prelude::*;
+#[godot_api]
+impl GameState {
+    #[func]
+    fn on_join_pressed(&mut self) {
+        godot_print!("Join clicked!");
+        self.screen = Screen::Game;
+    }
 
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = ["window", "godotMotion"], js_name = x)]
-    static MOTION_X: f64;
+    #[func]
+    fn on_create_pressed(&mut self) {
+        godot_print!("Create clicked!");
+        self.screen = Screen::Host;
+    }
 
-    #[wasm_bindgen(js_namespace = ["window", "godotMotion"], js_name = y)]
-    static MOTION_Y: f64;
-
-    #[wasm_bindgen(js_namespace = ["window", "godotMotion"], js_name = z)]
-    static MOTION_Z: f64;
-
-    #[wasm_bindgen(js_namespace = ["window", "godotMotion"], js_name = is_mobile)]
-    static IS_MOBILE: bool;
+    #[func]
+    fn on_spectate_pressed(&mut self) {
+        godot_print!("Spectate clicked!");
+        self.screen = Screen::Game;
+    }
 }
 
 #[cfg(target_arch = "wasm32")]
-fn browser_accel() -> godot::builtin::Vector3 {
-    unsafe { godot::builtin::Vector3::new(MOTION_X as f32, MOTION_Y as f32, MOTION_Z as f32) }
+mod WASM {
+    use wasm_bindgen::prelude::*;
+
+    #[wasm_bindgen]
+    extern "C" {
+        #[wasm_bindgen(js_namespace = ["window", "godotMotion"], js_name = x)]
+        static MOTION_X: f64;
+
+        #[wasm_bindgen(js_namespace = ["window", "godotMotion"], js_name = y)]
+        static MOTION_Y: f64;
+
+        #[wasm_bindgen(js_namespace = ["window", "godotMotion"], js_name = z)]
+        static MOTION_Z: f64;
+
+        #[wasm_bindgen(js_namespace = ["window", "godotMotion"], js_name = is_mobile)]
+        static IS_MOBILE: bool;
+    }
+
+    fn browser_accel() -> godot::builtin::Vector3 {
+        unsafe { godot::builtin::Vector3::new(MOTION_X as f32, MOTION_Y as f32, MOTION_Z as f32) }
+    }
 }
--- rust/crates/gdext/src/ui_manager.rs
+++ rust/crates/gdext/src/ui_manager.rs
@@ -1,11 +1,11 @@
 use godot::classes::{Control, IControl};
 use godot::prelude::*;
 
-use crate::game_state::GameState;
+use crate::game_state::Screen;
 
 #[derive(GodotClass)]
 #[class(base=Control)]
-struct UiManager {
+pub struct UiManager {
     base: Base<Control>,
 }
 
@@ -15,7 +15,29 @@ impl IControl for UiManager {
         Self { base }
     }
 
-    fn ready(&mut self) {
-        GameState::get();
+    fn ready(&mut self) {}
+}
+
+impl UiManager {
+    pub fn set_screen(&mut self, s: Screen, is_mobile: bool) {
+        let mut mobile = self
+            .base_mut()
+            .get_node_as::<Control>("CenterContainer/VBoxContainer/Mobile");
+        let mut desktop = self
+            .base_mut()
+            .get_node_as::<Control>("CenterContainer/VBoxContainer/Desktop");
+        match s {
+            Screen::MainMenu => {
+                if is_mobile {
+                    desktop.set_visible(false);
+                    mobile.set_visible(true);
+                } else {
+                    desktop.set_visible(true);
+                    mobile.set_visible(false);
+                }
+            }
+            Screen::Host => {}
+            Screen::Game => {}
+        }
     }
 }
