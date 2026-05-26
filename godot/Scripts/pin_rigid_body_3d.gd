8b3051ad48e4b4870c91871fa19e963112aeeced
Radu Ungureanu <radu.ungureanu@socopon.com>
2026-05-26 21:06:17 +0300

    make the game slifhtly easier
    Signed-off-by: Radu Ungureanu <radu.ungureanu@socopon.com>

--- godot/Scripts/pin_rigid_body_3d.gd
+++ godot/Scripts/pin_rigid_body_3d.gd
@@ -1,5 +1,5 @@
 extends RigidBody3D
 
 func _physics_process(_delta):
-	if global_transform.basis.y.dot(Vector3.UP) < 0.7 or global_position.y < -0.1:
+	if global_transform.basis.y.dot(Vector3.UP) < 0.85 or global_position.y < -0.1:
 		get_parent().on_pin_fell()
